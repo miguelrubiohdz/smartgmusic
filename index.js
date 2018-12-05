b6a7a0b226e16732db6771ca415a4b6db3de1266
Miguel Rubio <miguelantonio.rubio@gmail.com>
2018-12-05 00:43:51 -0700

    adds log test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-
+    console.log(request);
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
 
